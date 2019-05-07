5ad2041df32bd8fb557bd7129c44c752002effb4
Oliver Voeltz <frechmatz@gmx.de>
2019-05-07 20:46:09 +0200

    Readme-Generation now assumes globally installed html2markdown script

---
+++ make-readme/html2md.sh
@@ -0,0 +1,3 @@
+cl-readme-html2markdown \
+     /Users/olli/src/lisp/cl-wave-file-writer/make-readme/generated/readme.html \
+     /Users/olli/src/lisp/cl-wave-file-writer/make-readme/generated/readme.md
--- make-readme/index.js
+++
@@ -1,9 +0,0 @@
-var TurndownService = require('turndown');
-var fs = require('fs');
-
-var home = '/Users/olli/src/lisp/cl-wave-file-writer/';
-
-var htmlString = fs.readFileSync(home + 'make-readme/generated/readme.html', 'utf8');
-var turndownService = new TurndownService();
-var markdown = turndownService.turndown(htmlString);
-fs.writeFileSync(home + 'make-readme/generated/readme.md', markdown);
