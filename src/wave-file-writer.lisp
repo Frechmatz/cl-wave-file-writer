52734e18f682b69a1c8c8d2b566d5f3572e1f539
Oliver Voeltz <frechmatz@gmx.de>
2020-07-10 22:10:52 +0200

    Refactoring

--- src/wave-file-writer.lisp
+++ src/wave-file-writer.lisp
@@ -75,6 +75,28 @@
      value)))
 
 ;;
+;;
+;;
+
+(defparameter *sample-width-mapping*
+  (list
+   :8Bit
+   (list
+    :sample-width-bytes 1
+    ;; Unsigned representation for 8 bit waves
+    ;; https://en.wikipedia.org/wiki/WAV
+    ;; https://stackoverflow.com/questions/44415863/what-is-the-byte-format-of-an-8-bit-monaural-wav-file
+    :convert #'value-to-8bit-unsigned)
+   :16Bit
+   (list
+    :sample-width-bytes 2
+    :convert #'value-to-16bit-signed)
+   :24Bit
+   (list
+    :sample-width-bytes 3
+    :convert #'value-to-24bit-signed)))
+
+;;
 ;; Wave-File-Writer
 ;;
 
@@ -92,93 +114,80 @@
       <li>:close-file A function that closes the file.</li>
       <li>:write-sample A function that writes a sample. -1.0 <= sample <= 1.0.</li>
    </ul>"
-  (let ((sample-width-mapping
-	 (list
-	  :8Bit
-	  (list
-	   :sample-width-bytes 1
-	   ;; Unsigned representation for 8 bit waves
-	   ;; https://en.wikipedia.org/wiki/WAV
-	   ;; https://stackoverflow.com/questions/44415863/what-is-the-byte-format-of-an-8-bit-monaural-wav-file
-	   :convert #'value-to-8bit-unsigned)
-	  :16Bit
-	  (list
-	   :sample-width-bytes 2
-	   :convert #'value-to-16bit-signed)
-	  :24Bit
-	  (list
-	   :sample-width-bytes 3
-	   :convert #'value-to-24bit-signed))))
-    (let* ((sample-count 0)
-	   (file-output-stream nil)
-	   (sample-mapping (getf sample-width-mapping sample-width))
-	   (sample-width-bytes (getf sample-mapping :sample-width-bytes))
-	   (convert-sample (getf sample-mapping :convert)))
-      (labels ((open-file ()
-		 (format t "~%Open file ~a~%" filename)
-		 (setf file-output-stream
-		       (open
-			filename
-			:element-type 'unsigned-byte
-			:direction :io
-			:if-exists :supersede
-			:if-does-not-exist :create)))
-	       (close-file ()
-		 (if file-output-stream
-		     (progn
-		       (format t "~%Close file ~a~%" filename)
-		       (close file-output-stream)
-		       (setf file-output-stream nil))))
-	       (write-sample (value)
-		 (write-sint
-		  file-output-stream
-		  (funcall convert-sample value)
-		  sample-width-bytes))
-	       (get-fmt-chunk-size (number-of-samples)
-		 (declare (ignore number-of-samples))
-		 16)
-	       (get-data-chunk-size (number-of-samples)
-		 (* number-of-samples sample-width-bytes))
-	       (get-riff-chunk-size (number-of-samples)
-		 (+ 4 (get-data-chunk-size number-of-samples)
-		    (get-fmt-chunk-size number-of-samples)))
-	       (write-riff-chunk (number-of-samples)
-		 (let ((riff-size (get-riff-chunk-size number-of-samples)))
-		   (write-tag file-output-stream "RIFF")
-		   (write-uint file-output-stream riff-size 4)
-		   (write-tag file-output-stream "WAVE")))
-	       (write-format-chunk (number-of-samples)
-		 (let ((compression-code 1) ;; PCM
-		       ;; https://de.wikipedia.org/wiki/RIFF_WAVE
-		       (byte-rate (* sample-rate channel-count sample-width-bytes)))
-		   (write-tag file-output-stream "fmt ")
-		   (write-uint file-output-stream (get-fmt-chunk-size number-of-samples) 4)
-		   (write-uint file-output-stream compression-code 2)
-		   (write-uint file-output-stream channel-count 2)
-		   (write-uint file-output-stream sample-rate 4)
-		   (write-uint file-output-stream byte-rate 4)
-		   (write-uint file-output-stream sample-width-bytes 2)
-		   (write-uint file-output-stream (* sample-width-bytes 8) 2)))
-	       (write-data-chunk (number-of-samples)
-		 (let ((data-size (get-data-chunk-size number-of-samples)))
-		   (write-tag file-output-stream "data")
-		   (write-uint file-output-stream data-size 4))))
-	(list
-	 :open-file (lambda ()
-		      (open-file)
-		      ;; Write preliminary chunks
-		      (write-riff-chunk 0)
-		      (write-format-chunk 0)
-		      (write-data-chunk 0))
-	 :write-sample (lambda (sample)
-			 "sample: -1.0 ... 1.0"
-			 (setf sample-count (+ 1 sample-count))
-			 (write-sample sample))
-	 :close-file (lambda ()
-		       ;; Seek to start of file and update chunks
-		       (file-position file-output-stream :start)
-		       (write-riff-chunk sample-count)
-		       (write-format-chunk sample-count)
-		       (write-data-chunk sample-count)
-		       (close-file)))))))
+  (let* ((sample-count 0)
+	 (file-output-stream nil)
+	 (sample-mapping (getf *sample-width-mapping* sample-width))
+	 (sample-width-bytes (getf sample-mapping :sample-width-bytes))
+	 (convert-sample (getf sample-mapping :convert)))
+    (if (< channel-count 1)
+	(error (format nil "Channel count must be greater zero: ~a" channel-count)))
+    (if (not sample-mapping)
+	(error (format nil "Unsupported sample-width: ~a" sample-width)))
+    (labels ((open-file ()
+	       (format t "~%Open file ~a~%" filename)
+	       (setf file-output-stream
+		     (open
+		      filename
+		      :element-type 'unsigned-byte
+		      :direction :io
+		      :if-exists :supersede
+		      :if-does-not-exist :create)))
+	     (close-file ()
+	       (if file-output-stream
+		   (progn
+		     (format t "~%Close file ~a~%" filename)
+		     (close file-output-stream)
+		     (setf file-output-stream nil))))
+	     (write-sample (value)
+	       (write-sint
+		file-output-stream
+		(funcall convert-sample value)
+		sample-width-bytes))
+	     (get-fmt-chunk-size (number-of-samples)
+	       (declare (ignore number-of-samples))
+	       16)
+	     (get-data-chunk-size (number-of-samples)
+	       (* number-of-samples sample-width-bytes))
+	     (get-riff-chunk-size (number-of-samples)
+	       (+ 4 (get-data-chunk-size number-of-samples)
+		  (get-fmt-chunk-size number-of-samples)))
+	     (write-riff-chunk (number-of-samples)
+	       (let ((riff-size (get-riff-chunk-size number-of-samples)))
+		 (write-tag file-output-stream "RIFF")
+		 (write-uint file-output-stream riff-size 4)
+		 (write-tag file-output-stream "WAVE")))
+	     (write-format-chunk (number-of-samples)
+	       (let ((compression-code 1) ;; PCM
+		     ;; https://de.wikipedia.org/wiki/RIFF_WAVE
+		     (byte-rate (* sample-rate channel-count sample-width-bytes)))
+		 (write-tag file-output-stream "fmt ")
+		 (write-uint file-output-stream (get-fmt-chunk-size number-of-samples) 4)
+		 (write-uint file-output-stream compression-code 2)
+		 (write-uint file-output-stream channel-count 2)
+		 (write-uint file-output-stream sample-rate 4)
+		 (write-uint file-output-stream byte-rate 4)
+		 (write-uint file-output-stream sample-width-bytes 2)
+		 (write-uint file-output-stream (* sample-width-bytes 8) 2)))
+	     (write-data-chunk (number-of-samples)
+	       (let ((data-size (get-data-chunk-size number-of-samples)))
+		 (write-tag file-output-stream "data")
+		 (write-uint file-output-stream data-size 4))))
+      (list
+       :open-file (lambda ()
+		    (open-file)
+		    ;; Write preliminary chunks
+		    (write-riff-chunk 0)
+		    (write-format-chunk 0)
+		    (write-data-chunk 0))
+       :write-sample (lambda (sample)
+		       "sample: -1.0 ... 1.0"
+		       (setf sample-count (+ 1 sample-count))
+		       (write-sample sample))
+       :close-file (lambda ()
+		     ;; Seek to start of file and update chunks
+		     (file-position file-output-stream :start)
+		     (write-riff-chunk sample-count)
+		     (write-format-chunk sample-count)
+		     (write-data-chunk sample-count)
+		     (close-file))))))
 
